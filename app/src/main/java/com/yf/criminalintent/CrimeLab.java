e1f0efd855b459b9721e662c59e4cc62a24011e3
yafrees <yafree18@gmail.com>
2016-06-12 21:56:42 +0800

    修改代码（Page171）

--- app/src/main/java/com/yf/criminalintent/CrimeLab.java
+++ app/src/main/java/com/yf/criminalintent/CrimeLab.java
@@ -18,7 +18,7 @@ public class CrimeLab {
         mCrimes = new ArrayList<>();
         for (int i = 0 ; i < 100 ; i++){
             Crime crime = new Crime();
-            crime.setTitle("Crime ---" + i);
+            crime.setTitle("Crime # " + i);
             crime.setSolved(i % 2 == 0);
             mCrimes.add(crime);
         }
