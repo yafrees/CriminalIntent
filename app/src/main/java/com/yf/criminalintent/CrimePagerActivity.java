4c252d84a94643cf81e232b12de6596ddaadb04e
yafrees <yafree18@gmail.com>
2016-06-13 20:21:52 +0800

    使用ViewPager，划屏切换显示Crime明细内容

---
+++ app/src/main/java/com/yf/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,49 @@
+package com.yf.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+
+/**
+ * Created by YF on 2016/6/13.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+
+    }
+
+
+
+
+}
