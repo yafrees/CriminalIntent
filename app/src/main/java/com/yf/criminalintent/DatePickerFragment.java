5e945c8a706127317567ae83730478acdf018381
yafrees <yafree18@gmail.com>
2016-06-13 20:56:19 +0800

    设置对话框的显示内容---日期

--- app/src/main/java/com/yf/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/yf/criminalintent/DatePickerFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by YF on 2016/6/13.
@@ -12,7 +14,11 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date , null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok , null)
                 .create();
