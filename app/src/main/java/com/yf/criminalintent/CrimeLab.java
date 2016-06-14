c1957ce6bb3263e6864909b313495ec547dfbdc6
yafrees <yafree18@gmail.com>
2016-06-14 20:09:23 +0800

    响应菜单项选择&实现层级式导航。（Page225）

--- app/src/main/java/com/yf/criminalintent/CrimeLab.java
+++ app/src/main/java/com/yf/criminalintent/CrimeLab.java
@@ -16,12 +16,13 @@ public class CrimeLab {
 
     private CrimeLab(Context context){
         mCrimes = new ArrayList<>();
-        for (int i = 0 ; i < 100 ; i++){
-            Crime crime = new Crime();
-            crime.setTitle("Crime # " + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
+        //可以点击菜单栏添加crime记录
+//        for (int i = 0 ; i < 100 ; i++){
+//            Crime crime = new Crime();
+//            crime.setTitle("Crime # " + i);
+//            crime.setSolved(i % 2 == 0);
+//            mCrimes.add(crime);
+//        }
     }
 
     public List<Crime> getCrimes(){
@@ -42,5 +43,10 @@ public class CrimeLab {
             sCrimeLab = new CrimeLab(context);
         }
         return sCrimeLab;
+    }
+
+    //手动添加crime记录
+    public void addCrime(Crime c){
+        mCrimes.add(c);
     }
 }
