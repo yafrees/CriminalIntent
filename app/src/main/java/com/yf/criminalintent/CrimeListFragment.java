ca27cd186af082f530f8a7cf5eefcee398ad5891
yafrees <yafree18@gmail.com>
2016-06-14 19:56:58 +0800

    创建工具栏菜单。

--- app/src/main/java/com/yf/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/yf/criminalintent/CrimeListFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -33,6 +35,18 @@ public class CrimeListFragment extends Fragment {
         updateUI();
 
         return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
     }
 
     @Override
@@ -126,7 +140,6 @@ public class CrimeListFragment extends Fragment {
             return mCrimes.size();
         }
     }
-
 
 
 }
