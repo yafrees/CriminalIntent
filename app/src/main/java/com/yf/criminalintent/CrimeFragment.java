a184e0e2651ec963772141cbb4b8faeef3062c1d
yafrees <yafree18@gmail.com>
2016-06-14 08:05:12 +0800

    chuan di shu ju gei DatePickerFragment(Page204)

--- app/src/main/java/com/yf/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/yf/criminalintent/CrimeFragment.java
@@ -83,7 +83,8 @@ public class CrimeFragment extends Fragment{
             @Override
             public void onClick(View v) {
                 FragmentManager manager = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+//                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
                 dialog.show(manager , DIALOG_DATE);
             }
         });
