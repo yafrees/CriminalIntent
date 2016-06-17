d1c70e80f4d5e7ec09c12e04df565567abded7f2
yafrees <yafree18@gmail.com>
2016-06-17 08:56:10 +0800

    zeng jia CrimeTable inner class CrimeDbSchema.class&& ding yi shu ju biao zi duan

---
+++ app/src/main/java/com/yf/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.yf.criminalintent.database;
+
+/**
+ * Created by YF on 2016/6/17.
+ */
+public class CrimeDbSchema {
+
+    public static final class CrimaTable{
+        public static final String NAME = "crimes";
+
+        public static final class Cols{
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
