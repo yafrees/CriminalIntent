53bc37b4fa4d0be1c7caa8c15d33e26fbb23ca29
yafrees <yafree18@gmail.com>
2016-06-28 19:37:49 +0800

    新增相机视图布局和标题视图布局&在fragment_crime中引入公共布局。

--- app/src/main/java/com/yf/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/yf/criminalintent/CrimeFragment.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.text.Editable;
